1a8ce424e38edc33fc158a685c90a243e3289475
gefracto <evgeniy.kostrika@gmail.com>
2017-07-26 13:53:59 +0300

    ura

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"html/template"
+	)
 
 func main() {
 	fmt.Println("Hello dudes! ")
+	http.HandleFunc("/", IndexHandler)
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+	t.ExecuteTemplate(w, "index", nil)
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
